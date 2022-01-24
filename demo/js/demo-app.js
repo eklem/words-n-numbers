0fa0055d5b35adea9aef32a739c9582198c9db42
Espen Klem <espen.klem@gmail.com>
2022-01-24 21:23:52 +0100

    Demo with regex selector

--- demo/js/demo-app.js
+++ demo/js/demo-app.js
@@ -1,12 +1,47 @@
 
-import $ from './blingbling.js'
 import wnn from './wnn.js'
 
-$('#querytext').on('input', queryText => {
-  // get textarea content
-  queryText = wnn.extract(queryText.target.value, { regex: wnn.wordsEmojis, toLowercase: true })
+// listen on textarea input
+document.getElementById('querytext').oninput = function () {
+  extract()
+}
+
+// listen to regex option
+document.getElementById('regex').oninput = function () {
+  extract()
+}
+
+// tokenize input
+const extract = function () {
+  // get query text
+  let queryText = document.getElementById('querytext').value
+  console.log(queryText)
+
+  // get regex type
+  let regexType = document.getElementById('regex').value
+  console.log(regexType)
+  regexType = getRegex(regexType)
+
+  // do regex according to type
+  const regexOption = { regex: regexType, toLowercase: true }
+  queryText = wnn.extract(queryText, regexOption)
+  console.log(queryText)
 
   // Populate div#wnn with tokenized text
   const node = document.createElement('span').innerText = JSON.stringify(queryText, 2, ' ')
   document.getElementById('wnn').replaceChildren(node)
-})
+}
+
+// choose correct regex
+const getRegex = function (value) {
+  if (value === 'words') { return wnn.words }
+  if (value === 'numbers') { return wnn.numbers }
+  if (value === 'emojis') { return wnn.emojis }
+  if (value === 'wordsNumbers') { return wnn.wordsNumbers }
+  if (value === 'wordsEmojis') { return wnn.wordsEmojis }
+  if (value === 'numberEmojis') { return wnn.numbersEmojis }
+  if (value === 'wordsNumbersEmojis') { return wnn.wordsNumbersEmojis }
+  if (value === 'tags') { return wnn.tags }
+  if (value === 'usernames') { return wnn.usernames }
+  if (value === 'email') { return wnn.email }
+}
